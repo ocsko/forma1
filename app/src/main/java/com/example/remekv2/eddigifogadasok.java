a29d17fc4fc579477ee013be726438ab7259314d
sz0hamtam <sz0hamtam@vasvari.org>
2022-03-29 08:35:35 +0200

    Eddigi fogadások oldal

---
+++ app/src/main/java/com/example/remekv2/eddigifogadasok.java
@@ -0,0 +1,14 @@
+package com.example.remekv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class eddigifogadasok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eddigifogadasok);
+    }
+}
