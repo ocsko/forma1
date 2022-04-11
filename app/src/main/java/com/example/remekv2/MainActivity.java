7cda0e0ab9ddcdb65bb7324e2b1aa8b38edadf3d
sz0moldan <sz0moldan@vasvari.org>
2022-04-11 09:05:50 +0200

    mainActivity

--- app/src/main/java/com/example/remekv2/MainActivity.java
+++ app/src/main/java/com/example/remekv2/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.remekv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnFogadas;
+    Button btnEddigiFogadasok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFogadas = findViewById(R.id.buttonFogadas);
+        btnEddigiFogadasok = findViewById(R.id.buttonEddigFogadasok);
+
+        btnFogadas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), fogadas.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnEddigiFogadasok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), eddigifogadasok.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
+
 }
